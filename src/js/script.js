d6bb40999785716a7165306f7dff1b3be3e79f61
uwu1202 <uwu40398@gmail.com>
2022-05-25 21:46:15 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplayHoverPause: true,
+        // nav: true,
+
+    });
+});
